2a55876c9b1647fc0728d531fc4c772ecddd3588
Ben-1327 <ff.walker1327@gmail.com>
2020-06-11 19:00:35 +0900

    Miss

--- db/migrate/20200610065743_add_devise_to_companies.rb
+++ db/migrate/20200610065743_add_devise_to_companies.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-class AddDeviseToCompanies < ActiveRecord::Migration[5.2]
-  def self.up
-    change_table :companies do |t|
+class DeviseCreateCompanies < ActiveRecord::Migration[5.2]
+  def change
+    create_table :companies do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
@@ -50,9 +50,9 @@ class AddDeviseToCompanies < ActiveRecord::Migration[5.2]
     # add_index :companies, :unlock_token,         unique: true
   end
 
-  def self.down
-    # By default, we don't want to make any assumption about how to roll back a migration when your
-    # model already existed. Please edit below which fields you would like to remove in this migration.
-    raise ActiveRecord::IrreversibleMigration
-  end
+  # def self.down
+  #   # By default, we don't want to make any assumption about how to roll back a migration when your
+  #   # model already existed. Please edit below which fields you would like to remove in this migration.
+  #   raise ActiveRecord::IrreversibleMigration
+  # end
 end
